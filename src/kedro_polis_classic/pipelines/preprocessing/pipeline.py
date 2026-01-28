e38934a7028f87710d562c0d2c77844ea1d017c1
Patrick Connolly <patrick.c.connolly@gmail.com>
2026-01-27 19:49:09 -0500

    Initial commit

---
+++ src/kedro_polis_classic/pipelines/preprocessing/pipeline.py
@@ -0,0 +1,68 @@
+from kedro.pipeline import Pipeline, node
+from reddwarf.utils.matrix import deduplicate_votes
+from ..experimental.nodes import (
+    load_polis_data,
+    make_raw_vote_matrix,
+    make_participant_mask,
+    make_statement_mask,
+    make_masked_vote_matrix,
+)
+
+
+def create_pipeline() -> Pipeline:
+    """
+    Create the preprocessing pipeline that runs up to masked_vote_matrix.
+
+    This pipeline handles all the data loading and preprocessing steps that
+    are common to all experimental pipelines, producing the masked_vote_matrix
+    and related outputs that other pipelines can use as inputs.
+
+    Returns:
+        Pipeline: A Kedro pipeline containing all preprocessing nodes
+    """
+    nodes = [
+        # Data loading nodes
+        node(
+            func=load_polis_data,
+            inputs=[
+                "params:base_url",
+                "params:polis_url",
+                "params:import_dir",
+            ],
+            outputs=["raw_votes", "raw_comments"],
+            name="load_polis_data",
+        ),
+        node(
+            func=deduplicate_votes,
+            inputs="raw_votes",
+            outputs=["deduped_votes", "_duplicates"],
+            name="dedup_votes",
+        ),
+        node(
+            func=make_raw_vote_matrix,
+            inputs="deduped_votes",
+            outputs="raw_vote_matrix",
+            name="make_raw_vote_matrix",
+        ),
+        # Preprocessing nodes
+        node(
+            func=make_participant_mask,
+            inputs=["raw_vote_matrix", "params:min_votes_threshold"],
+            outputs="participant_mask",
+            name="make_participant_mask",
+        ),
+        node(
+            func=make_statement_mask,
+            inputs=["raw_comments"],
+            outputs="statement_mask",
+            name="make_statement_mask",
+        ),
+        node(
+            func=make_masked_vote_matrix,
+            inputs=["raw_vote_matrix", "statement_mask"],
+            outputs="masked_vote_matrix",
+            name="make_masked_vote_matrix",
+        ),
+    ]
+
+    return Pipeline(nodes)
